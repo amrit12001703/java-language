acad4416c327470ff87924f1e9d4e32c878b1b49
amrit12001703 <amrit.12001703@lpu.in>
2023-02-02 22:47:05 +0530

    Array Intro

---
+++ ArrayIntro.java
@@ -0,0 +1,8 @@
+public class ArrayIntro {
+    public static void main(String []args){
+        int []a={10,20,30,40};
+        //length: is a member of array
+        System.out.println(a.length);
+        System.out.println(a[0]);
+    }
+}
